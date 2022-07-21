466396cf7b9795b4a3116d3923057c5789bed775
PlamenD <plamen.d@protonmail.com>
2022-07-22 02:27:02 +0300

    figuring out the images aaa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const path = require('path');
+require('dotenv').config();
 
 const posts = require('./routes/posts');
 
